bed1059fbaff9c58983c47cc1ee9e5c4e0ae6e27
viv <vivian_pr@hotmail.com>
2018-05-02 20:02:10 +0800

    added solidity and react code

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Web3 from 'web3';
-import lottery from './lottery'
+import web3 from './web3';
+import lottery from './lottery';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    state = {
+      manager: '',
+      players: [],
+      balance: '',
 
-    this.state =  { manager: ''};
-  }
+    }
 
   async componentDiMount() {
+     // no need to specify call address as it's coming from metamask account
      const manager = await lottery.methods.manager().call();
+     const players = await lottery.methods.getPlayers().call();
+     const balance = await web3.eth.getBalance(lottery.options.address);
 
-     this.setState({ manager });
+
+     this.setState({ manager, players, balance });
   }
 
 
@@ -23,6 +27,11 @@ class App extends Component {
         <div>
           <h2>Lottery Contract</h2>
             <p>This contract is managed by {this.state.manager}</p>
+               <p>
+                This contract is managed by {this.state.manager}. There are currently{' '}
+                {this.state.players.length} people entered, competing to win{' '}
+                {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+              </p>
         </div>
     );
   }
